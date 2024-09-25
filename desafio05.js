65eb0636ef73bf37614164735046a58287532d7f
Luis Felipe R . L <luisramires29032002@gmail.com>
2024-09-25 01:50:10 -0300

    desafio 05 concluido

---
+++ desafio05.js
@@ -0,0 +1,10 @@
+// Primeiro, você liga o primeiro interruptor (vamos chamar de Interruptor 1) e deixa ele ligado por um tempinho pra esquentar a lâmpada.
+
+// Depois de alguns minutos, você desliga o Interruptor 1 e liga o segundo (Interruptor 2). Agora é a hora de ir até a sala das lâmpadas.
+
+// Quando você chegar lá, vai ver o seguinte:
+
+// A lâmpada que estiver acesa é ligada pelo Interruptor 2, porque você deixou ele ligado.
+// A lâmpada que estiver apagada mas quente é do Interruptor 1, porque ela esquentou enquanto estava ligada e esfriou um pouco depois.
+// A lâmpada apagada e fria? É do Interruptor 3, que você nem mexeu.
+// Com essa jogada esperta, você descobre qual interruptor acende qual lâmpada com uma só ida até a sala das lâmpadas
